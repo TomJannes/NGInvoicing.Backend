1d44c6d50cc60d3d5e520d8dfd490cf785f3a63a
Tom Jannes <tom.jannes@gmail.com>
2018-01-21 16:36:20 +0100

    invoice number implementation

--- api/invoice/model/invoice.js
+++ api/invoice/model/invoice.js
@@ -10,12 +10,34 @@ const InvoiceSchema = new Schema({
     _id: { type: Schema.ObjectId, auto: true },
     customer: CustomerModel.schema,
     number: { type: Number, required: false },
+    invoiceDate: { type: Date, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
     total: { type: Number, required: true },
     totalInc: { type: Number, required: true },
     totalVat: { type: Number, required: true },
     lines: [InvoiceLineModel.schema]
+});
+
+InvoiceSchema.pre('save', function (next) {
+    var invoice = this;
+    const invoiceYear = this.invoiceDate.getFullYear();
+    const lowerbound = invoiceYear * 1000;
+    const upperbound = (invoiceYear + 1) * 1000;
+    this.constructor.findOne({ number: { $gt: lowerbound, $lt: upperbound } })
+        .sort({createdAt: -1})
+        .exec()
+        .then(function(result) {
+            if(result === null) {
+                invoice.number = lowerbound + 1;
+            } else {
+                invoice.number = result.number + 1;
+            }
+            next();
+        })
+        .catch(function(err){
+            return next(err);
+        })
 });
 
 module.exports = mongoose.model('Invoice', InvoiceSchema);
