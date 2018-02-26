81e3ee1b44af0fff580d69bbde2969182569ac1d
Tom Jannes <tom.jannes@gmail.com>
2018-02-26 23:09:08 +0100

    Change port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const logger = require('./utils/logger');
 const errorHandling = require('./utils/errorHandling');
 const passport = require('passport');
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
