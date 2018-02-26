d9ce4b934bc177100ab44223ca464757b4274187
Tom Jannes <tom.jannes@gmail.com>
2018-02-26 23:16:11 +0100

    temp remove auth

--- api/sku/routes.js
+++ api/sku/routes.js
@@ -6,13 +6,13 @@ module.exports = function (app) {
   var sku = require('./controller/sku');
 
   app.route('/sku')
-    .all(passport.authenticate("jwt", { session: false }))
+    //.all(passport.authenticate("jwt", { session: false }))
     .get(sku.findSkus)
     .post(sku.createSku);
 
 
   app.route('/sku/:skuId')
-    .all(passport.authenticate("jwt", { session: false }))
+    //.all(passport.authenticate("jwt", { session: false }))
     .get(sku.getSku)
     .put(sku.updateSku)
     .delete(sku.deleteSku);
